696886d24f0492e7b5c925598680bf8fdd491664
sidman021 <siddusidhartha931@gmail.com>
2024-04-26 15:37:18 +0530

    Completed Nav bar desktop and mobile

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("JS Connected")
+
+function Menu(){
+    const menu = document.querySelector(".menu-link"); 
+    const icon =document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
